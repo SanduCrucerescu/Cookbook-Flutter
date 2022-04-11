83fce5b496a273e1b24615a8f56839c5393f00fe
Ingmar Falk <if222gq@student.lnu.se>
2022-04-11 11:34:46 +0200

    Deleting useless files

---
+++ lib/db/database_manager.dart
@@ -0,0 +1,132 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:mysql1/mysql1.dart';
+
+abstract class AbstractDatabaseManager {
+  final String host;
+  final String user;
+  final String password;
+  final String db;
+  final int? port;
+  final ConnectionSettings settings;
+
+  AbstractDatabaseManager({
+    required this.host,
+    required this.user,
+    required this.password,
+    required this.db,
+    this.port,
+  }) : settings = ConnectionSettings(
+            host: host,
+            user: user,
+            password: password,
+            db: db,
+            port: port ?? 3306);
+
+  void insert({
+    required String table,
+    required List<Map<String, dynamic>> params,
+  });
+
+  void update({
+    required String table,
+    required Map<String, dynamic> params,
+    required String where,
+  });
+
+  void delete({
+    required String table,
+    required Map<String, dynamic> where,
+  });
+
+  void select({
+    required String table,
+    required List<String> fields,
+    required Map<String, dynamic> where,
+    String group,
+    String having,
+    List<int> limit,
+  });
+
+  Future<void> connect();
+
+  Future<void> close();
+
+  Future<void> query({required String query});
+}
+
+class DatabaseManager extends AbstractDatabaseManager {
+  MySqlConnection? cnx;
+  late Results result;
+
+  DatabaseManager({
+    required String host,
+    required String user,
+    required String password,
+    required String db,
+  }) : super(host: host, user: user, password: password, db: db);
+
+  @override
+  Future<void> connect() async {
+    try {
+      cnx = await MySqlConnection.connect(settings);
+    } on SocketException catch (e) {
+      log("SocketException: " + e.message);
+    } on TimeoutException catch (e) {
+      log("TimeoutException: " + e.toString());
+    }
+  }
+
+  @override
+  Future<void> close() async {
+    await cnx?.close();
+  }
+
+  @override
+  Future<void> query({required String query}) async {
+    result = (await cnx?.query(query))!;
+  }
+
+  @override
+  Future<void> select(
+      {required String table,
+      required List<String> fields,
+      Map<String, dynamic>? where,
+      String? group,
+      String? having,
+      List<int>? limit}) async {
+    String query =
+        '''SELECT ${fields.length > 1 ? fields.join(", ") : fields[0]} FROM $table ''';
+
+    if (where != null) {
+      query += 'WHERE ';
+      for (MapEntry entry in where.entries) {
+        query += entry.key + " = " + entry.value + " ";
+      }
+    }
+    query += ";";
+
+    // result = await cnx?.query(query);
+  }
+
+  @override
+  void insert(
+      {required String table, required List<Map<String, dynamic>> params}) {
+    // TODO: implement insert
+  }
+
+  @override
+  void delete({required String table, required Map<String, dynamic> where}) {
+    // TODO: implement deleteFrom
+  }
+
+  @override
+  void update(
+      {required String table,
+      required Map<String, dynamic> params,
+      required String where}) {
+    // TODO: implement update
+  }
+}
