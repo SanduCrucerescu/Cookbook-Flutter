790c98971e5a00955d22ca0e67bab91cc5a38801
Nikita Grishin <ng222jk@student.lnu.se>
2022-04-15 17:54:35 +0200

    stuff

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -4,7 +4,6 @@
 
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
-// ignore_for_file: depend_on_referenced_packages
 
 import 'package:file_selector_web/file_selector_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
