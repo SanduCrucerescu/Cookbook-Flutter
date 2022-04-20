939719a56e371047f156a76729db9a72965551f2
Sandu Crucerescu <sc222tf@student.lnu.se>
2022-04-20 18:02:19 +0200

    Add recipe UI ~80% done

--- lib/pages/recipeadd/ui_components.dart
+++ lib/pages/recipeadd/ui_components.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 import 'package:cookbook/components/components.dart';
 import 'package:cookbook/pages/register/register.dart';
 import 'package:cookbook/theme/colors.dart';
-import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
