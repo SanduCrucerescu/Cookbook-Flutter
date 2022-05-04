90ba0bd07af8426c6b9a5314ac5b4aade692da27
Ingmar Falk <if222gq@student.lnu.se>
2022-05-04 11:31:54 +0200

    Fixed Recipe display when number of recipes is seven

--- lib/pages/favorites/favorites.dart
+++ lib/pages/favorites/favorites.dart
@@ -1,12 +1,10 @@
 import 'dart:developer';
-
 import 'package:cookbook/components/components.dart';
 import 'package:cookbook/controllers/get_favorites.dart';
 import 'package:cookbook/models/recipe/recipe.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
 import '../../main.dart';
 
 class FavoritesPage extends StatefulHookConsumerWidget {
@@ -44,7 +42,7 @@ class _FavoritesPageState extends ConsumerState<FavoritesPage> {
 
   @override
   void initState() {
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) async {
       await getFavorites
           .getfav(InheritedLoginProvider.of(context).userData?['email']);
       setState(() {});
@@ -115,7 +113,7 @@ class ResponsiveNotifier extends ChangeNotifier {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: List.generate(
             i + cols > displayedRecipes.length
-                ? cols - displayedRecipes.length % cols
+                ? cols - (displayedRecipes.length % cols + 1)
                 : cols,
             (idx) => Center(
               child: RecipeBox(
