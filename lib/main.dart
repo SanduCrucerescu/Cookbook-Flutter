f20b499c75e35d072d5386adcf8973c7383fc4d9
Sandu Crucerescu <sc222tf@student.lnu.se>
2022-05-04 12:35:43 +0200

    Closing the conections

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'package:cookbook/controllers/controllers.dart';
 import 'package:cookbook/models/recipe/recipe.dart';
+import 'package:cookbook/pages/userPage/user_page.dart';
 import 'package:cookbook/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -137,7 +138,7 @@ class App extends StatelessWidget {
           fontFamily: 'Montserrat',
           primaryColor: kcMedBeige,
         ),
-        initialRoute: UserPage.id,
+        initialRoute: LoadingScreen.id,
         onGenerateRoute: RouteGenerator.generateRoute,
       ),
     );
