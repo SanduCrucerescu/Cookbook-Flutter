91703b74052ed6032f0d11c032e4658e577a2186
Nikita Grishin <ng222jk@student.lnu.se>
2022-05-04 12:42:25 +0200

    fixed

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -16,13 +16,8 @@ import 'package:cookbook/pages/home/home_page.dart';
 import 'package:cookbook/pages/loading/loading_page.dart';
 import 'package:cookbook/pages/login/login.dart';
 import 'package:cookbook/pages/messages/message_screen.dart';
-<<<<<<< HEAD
 import 'package:cookbook/pages/recipeadd/ui_components.dart';
-import 'package:cookbook/pages/shoppingCart/shoppingCart.dart';
-=======
-import 'package:cookbook/pages/recipeadd/recipe_add.dart';
 import 'package:cookbook/pages/shoppingCart/shoppingPage.dart';
->>>>>>> 2f07b58 (shopping cart proggress)
 import 'package:cookbook/pages/userPage/user_page.dart';
 import 'package:cookbook/theme/colors.dart';
 import 'package:cookbook/theme/text_styles.dart';
@@ -31,7 +26,6 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../pages/shoppingCart/shoppingCart.dart';
 part 'custom_text_field.dart';
 part 'custom_text_field_notifier.dart';
 part 'custom_button.dart';
