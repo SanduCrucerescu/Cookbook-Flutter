779482c536e2ec296aa4fc5f795cca0193d91141
Mamoun Abu-Naseer <ma226gy@student.lnu.se>
2022-05-06 17:28:27 +0200

    Fixed sizing of icons

--- lib/pages/userPage/profile_widget.dart
+++ lib/pages/userPage/profile_widget.dart
@@ -73,11 +73,11 @@ class ProfilePictureWidget extends StatelessWidget {
       padding: scale == null ? 4 : 4 * scale!,
       child: CircleWidget(
         color: Colors.black,
-        padding: scale == null ? 10 : 10 * scale!,
+        padding: scale == null ? 10 : 5 * scale!,
         child: Icon(
           Icons.add_a_photo,
           color: Colors.white,
-          size: scale == null ? 25 : 25 * scale!,
+          size: scale == null ? 25 : 15 * scale!,
         ),
       ),
     );
