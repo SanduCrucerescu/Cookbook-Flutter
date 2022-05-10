859b58c939d3037a54f972d0a0f554804b347cae
Nikita Grishin <ng222jk@student.lnu.se>
2022-05-10 15:22:07 +0200

    smol changes

--- lib/db/database_manager.dart
+++ lib/db/database_manager.dart
@@ -103,6 +103,7 @@ class DatabaseManager extends AbstractDatabaseManager {
     await connect();
     print(query);
     result = await cnx!.query(query);
+    cnx!.close();
     return result;
   }
 
